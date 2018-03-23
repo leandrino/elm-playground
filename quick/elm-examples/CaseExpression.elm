a58a14185216f85fbe7040785c217af77bef4aa2
Leandro Alexandrino <alexandrinowd@gmail.com>
2018-03-22 23:01:59 -0300

    added case expression in elm

---
+++ quick/elm-examples/CaseExpression.elm
@@ -0,0 +1,54 @@
+module CaseExpression exposing (..)
+
+import Html
+
+weekday dayInNumber =
+    case dayInNumber of
+        0 ->
+            "Sunday"
+
+        1 ->
+            "Monday"
+
+        2 ->
+            "Tuesday"
+
+        3 ->
+            "Wednesday"
+
+        4 ->
+            "Thursday"
+
+        5 ->
+            "Friday"
+
+        6 ->
+            "Saturday"
+
+        _ ->
+            "Unknown day"
+
+hashtag dayInNumber =
+    case weekday dayInNumber of
+        "Sunday" ->
+            "#SinDay"
+        "Monday" ->
+            "#MondayBlues"
+        "Tuesday" ->
+            "#TakeMeBackTuesday"
+        "Wednesday" ->
+            "HumpDay"
+        "Thursday" ->
+            "#ThrowbackThursday"
+        "Friday" ->
+            "#FlashbackFriday"
+        "Saturday" ->
+            "#Caturday"
+        _ ->
+            "#Whatever"
+
+
+main =
+    hashtag 5
+        |> Html.text
+
